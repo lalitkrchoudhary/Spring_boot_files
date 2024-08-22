645d7bdf4e7fb5cea49fb1656ef6f67ce21ee4d9
lalit8694 <lalit8694123@gmail.com>
2024-08-22 09:28:49 +0530

    LOg4j files

---
+++ log4j01/src/main/java/in/ineuron/JdbcApp.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+/**
+ * Hello world!
+ *
+ */
+public class JdbcApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
