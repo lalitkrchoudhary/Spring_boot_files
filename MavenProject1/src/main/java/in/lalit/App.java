e9992078c9da5d5c434577c2925b5e3317399c4d
lalit8694 <lalit8694123@gmail.com>
2024-09-06 15:29:25 +0530

    lombok

--- MavenProject1/src/main/java/in/lalit/App.java
+++ MavenProject1/src/main/java/in/lalit/App.java
@@ -1,5 +1,7 @@
 package in.lalit;
 
+import in.lalit.bo.Studentbo;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello lalit world" );
+        Studentbo std= new Studentbo();
+       
     }
 }
---
+++ MavenProject1/src/main/java/in/lalit/bo/Studentbo.java
@@ -0,0 +1,14 @@
+package in.lalit.bo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Studentbo {
+
+	private String name;
+
+	
+	
+}
